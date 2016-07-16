51dbb664414db902d86496253344409e59bd6675
Ruy R. Garcia <rrgarciach@gmail.com>
2016-07-16 12:25:00 -0500

    Added gulp injector.

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,24 @@
-var gulp = require('gulp');
-var gutil = require('gulp-util');
-var bower = require('bower');
-var concat = require('gulp-concat');
-var sass = require('gulp-sass');
-var minifyCss = require('gulp-minify-css');
-var rename = require('gulp-rename');
-var sh = require('shelljs');
+const gulp = require('gulp');
+const gutil = require('gulp-util');
+const bower = require('bower');
+const concat = require('gulp-concat');
+const sass = require('gulp-sass');
+const minifyCss = require('gulp-minify-css');
+const rename = require('gulp-rename');
+const sh = require('shelljs');
+const inject = require('gulp-inject');
 
 require('./gulp/drakov');
 
-var paths = {
-  sass: ['./scss/**/*.scss']
+const paths = {
+  sass: ['./scss/**/*.scss'],
+  css: ['./www/assets/css/*.min.css'],
+  javascript: ['./www/app/**/*.js']
 };
 
 gulp.task('default', ['sass']);
 
-gulp.task('sass', function(done) {
+gulp.task('sass', function (done) {
   gulp.src('./scss/ionic.app.scss')
     .pipe(sass())
     .on('error', sass.logError)
@@ -23,23 +26,23 @@ gulp.task('sass', function(done) {
     .pipe(minifyCss({
       keepSpecialComments: 0
     }))
-    .pipe(rename({ extname: '.min.css' }))
+    .pipe(rename({extname: '.min.css'}))
     .pipe(gulp.dest('./www/css/'))
     .on('end', done);
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', function () {
   gulp.watch(paths.sass, ['sass']);
 });
 
-gulp.task('install', ['git-check'], function() {
+gulp.task('install', ['git-check'], function () {
   return bower.commands.install()
-    .on('log', function(data) {
+    .on('log', function (data) {
       gutil.log('bower', gutil.colors.cyan(data.id), data.message);
     });
 });
 
-gulp.task('git-check', function(done) {
+gulp.task('git-check', function (done) {
   if (!sh.which('git')) {
     console.log(
       '  ' + gutil.colors.red('Git is not installed.'),
@@ -54,4 +57,12 @@ gulp.task('git-check', function(done) {
 
 gulp.task('default', () => {
   gulp.run('mock-server');
+});
+
+gulp.task('injector', function() {
+  return gulp.src('./www/index.html')
+    .pipe(inject(gulp.src(paths.css, {read: false}), {relative: true}))
+    .pipe(gulp.dest('./www'))
+    .pipe(inject(gulp.src(paths.javascript, {read: false}), {relative: true}))
+    .pipe(gulp.dest('./www'));
 });
